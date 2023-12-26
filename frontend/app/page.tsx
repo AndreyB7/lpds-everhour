2fdd9c9b05c3bc859e709769e04ef396d74f5f58
AndreyB7 <0129507@gmail.com>
2023-12-26 19:39:19 +0700

    deploy v0

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
     <Container>
       <Flex gap="2" align="center" direction="column">
         {session?.user ? <Text>Signed in as {session.user.email}</Text> : <Text>Not Signed In</Text>}
-        <Text>Dashboard</Text>
+        <Text>Dashboard v.0.0</Text>
       </Flex>
     </Container>
   )
