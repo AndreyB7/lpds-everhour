3dd97e85dc38682c12bec5a54ffecd288a28ef8d
AndreyB7 <0129507@gmail.com>
2023-12-26 21:31:21 +0700

    deploy upd

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ sudo npm ci &&
 cd frontend &&
 sudo npm ci &&
 cd .. &&
-sudo pm2 stop all &&
 sudo pm2 delete all &&
 sudo pm2 start "npm run start:server" --name server &&
-sudo pm2 start "npm run start:front" --name front;
+sudo pm2 start "npm run start:front" --name front
