b3ce5535c7820ea57382fe6021cc98525ef57f33
AndreyB7 <0129507@gmail.com>
2023-12-27 14:06:36 +0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ cd /home/g0129507/lpds-everhour;
 git pull &&
 npm i &&
 npm run build &&
-pm2 delete server || : &&
+pm2 delete server || true &&
 pm2 start build/server.js --name server &&
 cd frontend &&
 npm i &&
 npm run build &&
-pm2 delete front || : &&
+pm2 delete front || true &&
 pm2 start "npm run start" --name front
  
