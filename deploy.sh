460f335c63198917ea739b0179d78b93aa16539c
AndreyB7 <0129507@gmail.com>
2023-12-27 10:07:14 +0700

    deploy runner access

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 cd /home/g0129507/lpds-everhour;
-sudo su &&
-whoami
-#git pull &&
-#npm i &&
-#npm run build &&
-#pm2 delete server &&
-#pm2 start build/server.js --name server &&
-#cd frontend &&
-#npm i &&
-#npm run build &&
-#pm2 delete front &&
-#pm2 start "npm run start" --name front
+git pull &&
+npm i &&
+npm run build &&
+pm2 delete server || : &&
+pm2 start build/server.js --name server &&
+cd frontend &&
+npm i &&
+npm run build &&
+pm2 delete front || : &&
+pm2 start "npm run start" --name front
+ 
