4b8b2fe261a69016611b312a468924d37f4b9383
AndreyB7 <0129507@gmail.com>
2024-02-06 20:43:44 +0700

    hotfix deploy 8

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ rm -rf frontentd
 tar -xzf export.tar
 npm ci --prefix server
 cp .env.prod_frontend frontend/.env
-cp .env.prod_server server/.env
+cp .env.prod_server server/server/.env
 pm2 delete all
 cd server
-pm2 start server.js --name server
+pm2 start server/server.js --name server
 cd ..
 pm2 start frontend/server.js --name frontend
 rm export.tar run.sh
